e69e2a0b9ac246e0a9e7aa76b3dbe590e4530fb1
Arendra-Singh <167457615+Arendra-Singh@users.noreply.github.com>
2025-07-31 08:57:24 +0530

    Time: 5 ms (3.35%) | Memory: 55.3 MB (5.35%) - LeetSync

--- 27-remove-element/remove-element.js
+++ 27-remove-element/remove-element.js
@@ -1,14 +1,13 @@
 function removeElement(nums, val) {
-    let k = 0; // Counter for elements not equal to val
+    let k = 0;
 
     for (let i = 0; i < nums.length; i++) {
         if (nums[i] !== val) {
-            nums[k] = nums[i]; // Move the valid element to front
+            nums[k] = nums[i]; 
             k++;
         }
     }
 
-    // Output the changed array and count
     console.log("Updated array:", nums.slice(0, k));
     return k;
 }
